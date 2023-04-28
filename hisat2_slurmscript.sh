15a66859c46a26342c9cca85e31849863a79a76d
Zohar Steinberg <zohar.steinberg@rdm.ox.ac.uk>
2023-04-28 15:56:52 +0100

    Back up shell scripts from OBDS course genomics processing on command line

---
+++ hisat2_slurmscript.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+##########################################################################
+## A script template for submitting batch jobs. To submit a batch job, 
+## please type
+##
+##    sbatch myprog.sh
+##
+## Please note that anything after the characters "#SBATCH" on a line
+## will be treated as a Slurm option.
+##########################################################################
+
+## Specify a partition. For general use, the partition is "batch". 
+## Others are available for special purposes.
+## change to partition=batch after the course
+#SBATCH --partition=teaching
+
+## The following line will send an email notification to your registered email
+## address when the job ends or fails.
+#SBATCH --mail-type=END,FAIL
+
+## Specify the amount of memory that your job needs. This is for the whole job.
+## Asking for much more memory than needed will mean that it takes longer to
+## start when the cluster is busy.
+#SBATCH --mem=6G
+
+## Specify the number of CPU cores that your job can use. This is only relevant for
+## jobs which are able to take advantage of additional CPU cores. Asking for more
+## cores than your job can use will mean that it takes longer to start when the
+## cluster is busy.
+#SBATCH --ntasks=4
+
+## Specify the maximum amount of time that your job will need to run. Asking for
+## the correct amount of time can help to get your job to start quicker. Time is
+## specified as HOURS:MINUTES:SECONDS. This example is one week.
+#SBATCH --time=3:00:00
+
+## Capture the terminal output and save any error messages. This is very useful
+## if you have problems and need to ask for help.
+#SBATCH --output=%j_%x.out
+#SBATCH --error=%j_%x.err
+
+module load hisat2
+
+hisat2 --threads 4 \
+-x /databank/igenomes/Mus_musculus/Ensembl/GRCm38/Sequence/HISAT2/genome \
+-1 ERR1755083_1.fastq.gz \
+-2 ERR1755083_2.fastq.gz \
+--rna-strandness RF \
+--summary-file hisat2_run_stats.txt \
+-S ERR1755083.sam
---
+++ kallisto_quant_slurmscript.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+##########################################################################
+## A script template for submitting batch jobs. To submit a batch job, 
+## please type
+##
+##    sbatch myprog.sh
+##
+## Please note that anything after the characters "#SBATCH" on a line
+## will be treated as a Slurm option.
+##########################################################################
+
+## Specify a partition. For general use, the partition is "batch". 
+## Others are available for special purposes.
+## change to partition=batch after the course
+#SBATCH --partition=teaching
+
+## The following line will send an email notification to your registered email
+## address when the job ends or fails.
+#SBATCH --mail-type=END,FAIL
+
+## Specify the amount of memory that your job needs. This is for the whole job.
+## Asking for much more memory than needed will mean that it takes longer to
+## start when the cluster is busy.
+#SBATCH --mem=10G
+
+## Specify the number of CPU cores that your job can use. This is only relevant for
+## jobs which are able to take advantage of additional CPU cores. Asking for more
+## cores than your job can use will mean that it takes longer to start when the
+## cluster is busy.
+#SBATCH --ntasks=4
+
+## Specify the maximum amount of time that your job will need to run. Asking for
+## the correct amount of time can help to get your job to start quicker. Time is
+## specified as HOURS:MINUTES:SECONDS. This example is one week.
+#SBATCH --time=3:00:00
+
+## Capture the terminal output and save any error messages. This is very useful
+## if you have problems and need to ask for help.
+#SBATCH --output=%j_%x.out
+#SBATCH --error=%j_%x.err
+
+kallisto quant \
+-t 4 \
+--rf-stranded \
+-i mus_musculus_ensemble_index \
+-o kallisto-output-ERR1755083 \
+ERR1755083_1.fastq.gz ERR1755083_2.fastq.gz
+
+
+
